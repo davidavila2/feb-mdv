025b4bc47423d0b74ca91c70843a629a134a4b0c
david <david.avila250@yahoo.com>
2022-02-01 10:35:24 -0700

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsService } from './service/items.service';
+import { ItemsComponent } from './items/items.component';
+import { ItemListComponent } from './items/item-list/item-list.component';
+import { ItemDetailsComponent } from './items/item-details/item-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ItemListComponent,
+    ItemDetailsComponent
   ],
   imports: [
     AppRoutingModule,
